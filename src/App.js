54a2f41a02de0e4b497c2144bfd271b635315382
René Cortez <rene.cortes@outlook.com>
2023-04-25 12:18:48 -0600

    Set initialData on localstorage and call Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Header from './components/organisms/header';
+import initialData from "./assets/data/initialData";
+import './assets/scss/index.scss';
 
-function App() {
+const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("posts"));
+
+    if (!data) {
+      localStorage.setItem("posts", JSON.stringify(initialData));
+      setPosts(initialData);
+    } else {
+      setPosts(data);
+    }
+  }, []);
+
   return (
     <div className="App">
+      <Header />
     </div>
   );
-}
+};
 
 export default App;
