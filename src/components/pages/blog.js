32c9e865be583b5c1abbada9f81fc789a5148472
René Cortez <rene.cortes@outlook.com>
2023-04-25 16:35:42 -0600

    Add component and styles base for blog page

---
+++ src/components/pages/blog.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Blog = () => {
+    return (
+        <div className='container'>
+            <div className='blog'>
+                <h1>Blog Page</h1>
+                <h2>The standard Lorem Ipsum passage, used since the 1500s</h2>
+                <p className='blog__text'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Blog;
