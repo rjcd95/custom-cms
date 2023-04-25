8bca5438320b91b6782c74a7ebc2d69483f442a0
René Cortez <rene.cortes@outlook.com>
2023-04-25 15:31:22 -0600

    Update props in hero.js

--- src/components/molecules/hero.js
+++ src/components/molecules/hero.js
@@ -1,11 +1,12 @@
+import images from 'assets/data/images';
 import React from 'react';
 import { HeroImage, HeroText } from '../atoms/hero-image';
 
 const Hero = () => (
   <div className="hero">
     <HeroImage
-      imageUrl="https://cdn.wallpapersafari.com/68/66/KMgcJP.jpg"
-      altText="Hero Image"
+      imageUrl={images[0].url}
+      imageAlt="Hero Image"
     />
     <HeroText
       title="This is the Hero Image Title"
