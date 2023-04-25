7ef3bfbc48053d1d092cfe8544048e90f1a89d95
René Cortez <rene.cortes@outlook.com>
2023-04-25 17:28:52 -0600

    Retrieve posts from local storage
    - Show simple list

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Blog = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+      // retrieve posts from local storage
+      const storedPosts = localStorage.getItem("posts");
+      if (storedPosts) {
+        setPosts(JSON.parse(storedPosts));
+      }
+    }, []);
+
     return (
         <div className='container'>
             <div className='blog'>
                 <h1>Blog Page</h1>
-                <h2>The standard Lorem Ipsum passage, used since the 1500s</h2>
-                <p className='blog__text'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
+                <ul>
+                    {posts.map((post, index) => (
+                    <li key={index}>
+                        <h2>{post.title}</h2>
+                        <p>{post.content}</p>
+                    </li>
+                    ))}
+                </ul>
             </div>
         </div>
     );
