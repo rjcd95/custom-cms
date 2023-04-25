9db3c48b2a994b2b6e25bffd1a94243a85af924b
René Cortez <rene.cortes@outlook.com>
2023-04-25 12:04:46 -0600

    Add logo component

---
+++ src/components/atoms/logo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Logo = () => (
+  <div className="logo">
+    <img src="https://s0.wp.com/wp-content/themes/a8c/dotblog-refresh-2018/images/logo-dotBlog-2018.svg" alt="Logo" />
+  </div>
+);
+
+export default Logo;
