2ec3fdf0d393cc19957d24b6a63b596cc298e28c
René Cortez <rene.cortes@outlook.com>
2023-04-25 11:58:23 -0600

    Create initialData json

---
+++ src/assets/data/initialData.js
@@ -0,0 +1,14 @@
+const initialData = [
+    {
+      id: 1,
+      title: "First post",
+      body: "This is the first post on the site",
+    },
+    {
+      id: 2,
+      title: "Second post",
+      body: "This is the second post on the site",
+    },
+];
+  
+export default initialData;
