74e6497c62bddb850fa40e58e35764f4c4c226d1
René Cortez <rene.cortes@outlook.com>
2023-04-25 13:18:05 -0600

    Create home page

---
+++ src/components/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../organisms/header';
+import Hero from '../molecules/hero';
+
+const Home = () => {
+    return (
+        <div>
+            <Hero />
+        </div>
+    );
+};
+
+export default Home;
