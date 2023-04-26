64e734e4e4512e12cfd4bed754642ce89bf967bf
René Cortez <rene.cortes@outlook.com>
2023-04-26 15:12:47 -0600

    Allow click on matched results and adjust styles

--- src/components/molecules/search.js
+++ src/components/molecules/search.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { SearchLabel, SearchInput, SearchButton, SearchMatches } from '../atoms/search';
 import useLocalStorage from 'services/hooks/useLocalStorage';
 import { POSTS_KEY } from 'common/constants';
@@ -7,17 +7,18 @@ const Search = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [matches, setMatches] = useState([]);
   const [posts] = useLocalStorage(POSTS_KEY);
+  const ref = useRef(null);
 
   const handleSearchChange = (event) => {
     const searchTerm = event.target.value;
-    // filter posts by title or body containing the search term
+    // filter posts by title or content containing the search term
     const inputValue = event.target.value;
     const filteredPosts = posts.filter(
-      (post) =>
+      (post) => 
         post.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        post.body.toLowerCase().includes(searchTerm.toLowerCase())
+        post.content.toLowerCase().includes(searchTerm.toLowerCase())
     );
-  
+
     if (inputValue === '') {
       setMatches([]);
     } else {
@@ -32,15 +33,32 @@ const Search = () => {
     handleSearchChange({ target: { value: searchTerm } });
   }
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setMatches([]);
+      }
+    };
+    document.addEventListener('click', handleClickOutside, true);
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true);
+    };
+  }, []);
+
+
   return (
-    <div className="search">
+    <div ref={ref} className="search">
       <form className="search__form">
         <SearchLabel />
         <div>
           <SearchInput searchTerm={searchTerm} handleSearchChange={handleSearchChange}/>
           <SearchButton handleSubmit={handleSubmit} />
         </div>
-        <SearchMatches matches={matches} />
+        <SearchMatches
+          matches={matches}
+          setMatches={setMatches}
+          setSearchTerm={setSearchTerm}
+        />
       </form>
     </div>
   );
