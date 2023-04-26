c47d3ce20c161aeab918cd0b6375d0b7c749fed0
René Cortez <rene.cortes@outlook.com>
2023-04-25 20:52:04 -0600

    Update blog page and styles

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,31 +1,52 @@
-import React, { useState, useEffect } from 'react';
+import getExcerpt from "common/getExcerpt";
+import React, { useState, useEffect } from "react";
+import PostCard from "../atoms/postCard";
+import Pagination from "../molecules/pagination";
 
 const Blog = () => {
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(5);
 
-    useEffect(() => {
-      // retrieve posts from local storage
-      const storedPosts = localStorage.getItem("posts");
-      if (storedPosts) {
-        setPosts(JSON.parse(storedPosts));
-      }
-    }, []);
+  useEffect(() => {
+    const localPosts = JSON.parse(localStorage.getItem("posts"));
+    if (localPosts) {
+      setPosts(localPosts);
+    }
+  }, []);
 
-    return (
-        <div className='container'>
-            <div className='blog'>
-                <h1>Blog Page</h1>
-                <ul>
-                    {posts.map((post, index) => (
-                    <li key={index} className="post">
-                        <h2 className="post__title">{post.title}</h2>
-                        <p className="post__body">{post.body}</p>
-                    </li>
-                    ))}
-                </ul>
-            </div>
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  return (
+    <div>
+      <div className='container'>
+        <h1>Blog</h1>
+        <div className='blog'>
+          <div className='blog-posts'>
+            {currentPosts.map((post, index) => (
+                <PostCard key={index} post={{
+                    ...post,
+                    excerpt: getExcerpt(post.body, 100)
+                }}/>
+            ))}
+          </div>
+          <div className='blog-pagination'>
+            <Pagination
+              postsPerPage={postsPerPage}
+              totalPosts={posts.length}
+              paginate={paginate}
+              currentPage={currentPage}
+              setPostsPerPage={setPostsPerPage}
+            />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
