0dc02a854aed1965ef36b416bbba894730d28ebe
René Cortez <rene.cortes@outlook.com>
2023-04-26 15:19:27 -0600

    Add missing prop

--- src/components/atoms/search/searchInput.js
+++ src/components/atoms/search/searchInput.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SearchInput = ({ handleSearchChange }) => (
+const SearchInput = ({ handleSearchChange, searchTerm }) => (
     <input
         id="searchInput"
         className="search__input"
         type="text"
         placeholder="Search..."
+        value={searchTerm}
         onChange={handleSearchChange}
     />
 );
 
 SearchInput.propTypes = {
     handleSearchChange: PropTypes.func.isRequired,
+    searchTerm: PropTypes.string.isRequired,
 };
 
 export default SearchInput;
